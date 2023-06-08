64e0c9bcbbf65beb08506cc20444461289d7c945
Andrea <100373856+SerafiniAndrea@users.noreply.github.com>
2023-06-08 11:18:34 +0200

    Update Player.py
    update

--- Player.py
+++ Player.py
@@ -15,40 +15,49 @@ def seleziona_cartella():
 
 video_path = seleziona_video()
 
-video = cv2.VideoCapture(video_path)
-i = 0 #contatore screenshots
-frames = []
-j = 0 #contatore frames video capture
-
-if video.grab():
-        flag, frame = video.retrieve()
-        if flag:
-            frame = cv2.resize(frame, (1920, 1080))
-            frames.append(frame)
-            cv2.imshow('frame', frames[0])
-
-while True:
-    key = cv2.waitKey(0)
-    if key == 100:
-        if video.grab():
+if video_path:
+    video = cv2.VideoCapture(video_path)
+    i = 0 #contatore screenshots
+    frames = []
+    j = 0 #contatore frames video capture
+    
+    if video.grab():
             flag, frame = video.retrieve()
-            if not flag:
-                continue
-            else:
+            if flag:
                 frame = cv2.resize(frame, (1920, 1080))
                 frames.append(frame)
-                j = j + 1
+                cv2.imshow('frame', frames[0])
+
+    while True:
+        key = cv2.waitKey(0)
+        if key == 100:
+            if video.grab():
+                flag, frame = video.retrieve()
+                if not flag:
+                    continue
+                else:
+                    frame = cv2.resize(frame, (1920, 1080))
+                    frames.append(frame)
+                    j = j + 1
+                    cv2.imshow('frame', frames[j])
+        if key == 97:
+            if j > 0:
+                j = j - 1
                 cv2.imshow('frame', frames[j])
-    if key == 97:
-        if j > 0:
-            j = j - 1
-            cv2.imshow('frame', frames[j])
-        else:
-            continue
-    if key == 32:
-        cv2.imwrite('C:/Users/seraf/OneDrive/Desktop/VS Studio/PCTO/opencv/frames/frame'+str(i+1)+'.png', frame)
-        i = i + 1
-    if key == 27:
-        break
+            else:
+                continue
+        if key == 32:
+            if i == 0:
+                frame_path = seleziona_cartella()
+            if frame_path:
+                fourK = cv2.resize(frames[j], (3840, 2160))
+                cv2.imwrite(frame_path+'/frame'+str(i+1)+'.png', fourK)
+                i = i + 1
+            else:
+                print("Nessuna cartella selezionata.")
+        if key == 27:
+            break
+else:
+    print("Nessuna video selezionato.")
 
 video.release()
